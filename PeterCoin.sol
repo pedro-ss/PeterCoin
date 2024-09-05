142798a44c8b1a3e9be4408f24122e613279a336
PedroSS <pedross.tool@gmail.com>
2024-09-05 00:05:35 -0300

    Update PeterCoin.sol
    methods implementation

--- PeterCoin.sol
+++ PeterCoin.sol
@@ -23,5 +23,39 @@ contract PeterCoin is IERC20 {
     mapping(address=>mapping(address=>uint256)) allowed;
     uint256 totalSupply_ = 10 ether;
 
-    
+    constructor() {
+        balances[msg.sender] = totalSupply_;
+    }
+
+    function totalSupply() public override view returns (uint256){
+        return totalSupply_;
+    }
+    function balanceOf(address tokenOwner) public override view returns (uint256){
+        return balances[tokenOwner];
+    }
+    function tranfer(address receiver, uint256 numTokens) public override returns (bool){
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender] - numTokens;
+        balances[receiver] = balances[receiver] + numTokens;
+        emit Transfer(msg.sender, receiver, numTokens);
+        return true;
+    }
+    function approve(address delegate, uint256 numTokens) public override returns (bool){
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+    function allowance(address owner, address delegate) public override view returns (uint){
+        return allowed[owner][delegate];
+    }
+    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool){
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner] - numTokens;
+        allowed[owner][msg.sender] = allowed[owner][msg.sender] - numTokens;
+        emit Transfer(buyer, owner, numTokens);
+        return true;
+    }
+
 }
